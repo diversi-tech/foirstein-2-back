4d50c524d4d4f8e8f695d66b1d9f6421dae166fc
AyalaKelerman <149535284+AyalaKelerman@users.noreply.github.com>
2024-07-17 14:15:31 +0300

    Update IbllItem.cs

--- bll/IBll/IbllItem.cs
+++ bll/IBll/IbllItem.cs
@@ -14,7 +14,7 @@ namespace BLL.IBll
         Task<IEnumerable<BllItem>> ReadByString(String searchKey);
         Task<IEnumerable<BllItem>> ReadByCategory(String category);
         Task<IEnumerable<BllItem>> ReadByAttributes(BllItem searchItem);
-        Task<IEnumerable<BllItem>> ReadTheRecommended();
+        // Task<IEnumerable<BllItem>> ReadTheRecommended();
         Task<IEnumerable<BllItem>> ReadByTag(int tagId);
         Task<IEnumerable<BllItem>> ReadMostRequested();
 
