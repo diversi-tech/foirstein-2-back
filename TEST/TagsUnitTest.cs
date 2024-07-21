2d84fd6969448bb1703733ecc38f0bc385061ae0
RivkaFiga <rvk2994@gmail.com>
2024-07-21 11:54:43 +0300

    Add a test project

---
+++ TEST/TagsUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WEBAPI.Controllers;
+
+namespace TEST
+{
+    public class TagsUnitTest
+    {
+        static BlManager blManager = new BlManager();
+        static TagController tagController = new TagController(blManager);
+
+        [Fact]
+        public async Task TestGetAllTagsById_ReturnsEmptyListForUnknownId()
+        {
+            var tags = await tagController.GetAllTagsById(-1);
+            Assert.Empty(tags);
+        }
+    }
+}
